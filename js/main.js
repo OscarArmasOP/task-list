ec5c2ac1482dd05e1e0f09aa11e71d3dfe5a14cf
Oscar <oma_1506@hotmail.com>
2020-01-30 22:34:09 -0600

    Ultimo cambio del dia

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function() {
+    //Variables
+    var lista = document.getElementById("lista"),
+        tareaInput = document.getElementById("tareaInput"),
+        btnNuevaTarea = document.getElementById("btn-agregar");
+
+    //Functions
+    var agregarTarea = function() {};
+
+
+    //Events
+    //Agregar tarea
+    btnNuevaTarea.addEventListener("click", agregarTarea);
+
+    //Comprobar input
+    tareaInput.addEventListener("click", comprobarInput);
+}());
